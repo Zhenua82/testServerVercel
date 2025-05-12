4822775df4612b7c63ad118b30b8ccc8b719fe48
Zhenua82 <zzhmenka@yandex.ru>
2025-05-12 15:17:38 +0300

    adres

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('+++OK')
   res.writeHead(200, {
-    'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://ce03510-wordpress-og5g7.tw1.ru',
     'Content-Type': 'text/html',
   });
 
