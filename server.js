7cba8f0b1cd71d5933571714ed26076c1a7a51b3
Zhenua82 <zzhmenka@yandex.ru>
2025-05-12 15:30:56 +0300

    change

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   console.log('+++OK')
+//   res.writeHead(200, {
+//     // 'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Origin': ['https://ce03510-wordpress-og5g7.tw1.ru', 'http://127.0.0.1:5500'],
+//     'Content-Type': 'text/html',
+//   });
+
+//   res.end('<h1>Anse from server on port 5000!!!!!!!!!!!!</h1> <a href="#">Link</a>');
+// });
+
+// server.listen(5000, () => {
+//   console.log('Сервер запущен на порту 5000');
+// });
+
+
+
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  console.log('+++OK')
-  res.writeHead(200, {
-    // 'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Origin': ['https://ce03510-wordpress-og5g7.tw1.ru', 'http://127.0.0.1:5500'],
-    'Content-Type': 'text/html',
-  });
+const allowedOrigins = [
+  'https://ce03510-wordpress-og5g7.tw1.ru',
+  'http://127.0.0.1:5500'
+];
 
-  res.end('<h1>Anse from server on port 5000!!!!!!!!!!!!</h1> <a href="#">Link</a>');
+const server = http.createServer((req, res) => {
+  const origin = req.headers.origin;
+
+  // Проверяем, соответствует ли Origin разрешенным
+  if (allowedOrigins.includes(origin)) {
+    res.writeHead(200, {
+      'Access-Control-Allow-Origin': origin,
+      'Content-Type': 'text/html',
+    });
+  } else {
+    res.writeHead(403, {
+      'Content-Type': 'text/html',
+    });
+    res.end('<h1>403 Forbidden: Access is denied</h1>');
+    return;
+  }
+
+  console.log('+++OK');
+  res.end('<h1>Answer from server on port 5000!!!!!!!!!!!!</h1> <a href="#">Link</a>');
 });
 
 server.listen(5000, () => {
   console.log('Сервер запущен на порту 5000');
 });
-
