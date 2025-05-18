3be2bc3652f42ed34cbbc5d344c246fed63093ee
Zhenua82 <zzhmenka@yandex.ru>
2025-05-18 20:48:12 +0300

    new

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
+// const http = require('http');
+
+// const allowedOrigins = [
+//   'https://ce03510-wordpress-og5g7.tw1.ru',
+//   'http://127.0.0.1:5500',
+//   'https://testserver-eight-olive.vercel.app'
+// ];
+
+// const server = http.createServer((req, res) => {
+//   const origin = req.headers.origin;
+
+//   // Проверяем, соответствует ли Origin разрешенным
+//   if (allowedOrigins.includes(origin)) {
+//     res.writeHead(200, {
+//       'Access-Control-Allow-Origin': origin,
+//       'Content-Type': 'text/html',
+//     });
+//   } else {
+//     res.writeHead(403, {
+//       'Content-Type': 'text/html',
+//     });
+//     res.end('<h1>403 Forbidden: Access is denied</h1>');
+//     return;
+//   }
+
+//   console.log('+++OK');
+//   res.end('<h1>Answer from server on port 5000!!!!!!!!!!!!</h1> <a href="#">Link</a>');
+// });
+
+// server.listen(5000, () => {
+//   console.log('Сервер запущен на порту 5000');
+// });
+
 const http = require('http');
 
 const allowedOrigins = [
@@ -9,22 +42,20 @@ const allowedOrigins = [
 const server = http.createServer((req, res) => {
   const origin = req.headers.origin;
 
-  // Проверяем, соответствует ли Origin разрешенным
-  if (allowedOrigins.includes(origin)) {
+  if (!origin || allowedOrigins.includes(origin)) {
+    // Разрешаем доступ без проверки origin или если он разрешен
     res.writeHead(200, {
-      'Access-Control-Allow-Origin': origin,
       'Content-Type': 'text/html',
+      'Access-Control-Allow-Origin': origin || '*', // Можно поставить '*' для всех
     });
+    console.log('Allowed request from:', origin);
+    res.end('<h1>Answer from server on port 5000!!!!!!!!!!!!</h1> <a href="#">Link</a>');
   } else {
     res.writeHead(403, {
       'Content-Type': 'text/html',
     });
     res.end('<h1>403 Forbidden: Access is denied</h1>');
-    return;
   }
-
-  console.log('+++OK');
-  res.end('<h1>Answer from server on port 5000!!!!!!!!!!!!</h1> <a href="#">Link</a>');
 });
 
 server.listen(5000, () => {
